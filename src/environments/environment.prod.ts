4fa34b2d0fb3851113a9bd9ed6a40009c577814d
minhkhuong2404 <13156@student.vgu.edu.vn>
2022-03-06 10:49:51 +0700

    anguar exercises app deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAgsK1Is0QwGwhCQsKAm5nsAcYxcez6vGU',
-    authDomain: 'ng-fitness-tracker.firebaseapp.com',
-    databaseURL: 'https://ng-fitness-tracker.firebaseio.com',
-    projectId: 'ng-fitness-tracker',
-    storageBucket: 'ng-fitness-tracker.appspot.com',
-    messagingSenderId: '183546960271'
+    apiKey: 'AIzaSyCN2fnJ4gJ4ftTzYObpKJeqXQVlAwQMWpo',
+    authDomain: 'angular-exercise-33071.firebaseapp.com',
+    projectId: 'angular-exercise-33071',
+    storageBucket: 'angular-exercise-33071.appspot.com',
+    messagingSenderId: '478199925661',
+    appId: '1:478199925661:web:40fd23ba55e6c341ad3d74',
+    measurementId: 'G-FZVXGVQNKS'
   }
 };
